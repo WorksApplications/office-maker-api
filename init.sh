8ecfcc166a1255fbcc03d59b4b5f293c51e00ce3
seiichi <seiichi19881101@gmail.com>
2017-11-01 15:05:03 +0900

    create init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+npm install ./
+cd common
+npm install
+cd ../functions/authorizer/ 
+npm install
+cd ../../
+echo  "'aws:SourceIp':\n\t- 0.0.0.0/0" > sourceIp.yaml
+echo "" > functions/authorizer/pubkey.pem
+echo "" > functions/authorizer/guest_token.json
+
+

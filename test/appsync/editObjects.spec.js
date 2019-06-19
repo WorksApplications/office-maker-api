1202699cbb406a8ed9a7d9fb4a7295bbfae2970e
ishibashi_y <ishibashi_y@worksap.co.jp>
2019-06-19 17:15:34 +0900

    chore: use node-fetch

--- test/appsync/editObjects.spec.js
+++ test/appsync/editObjects.spec.js
@@ -1,22 +1,29 @@
-const expect = require('chai').expect;
-const axios = require('axios').default;
-const awsconfig = require('../../src/aws-exports').default;
-const { AUTH_TYPE } = require('aws-appsync');
-const AWSAppSyncClient = require('aws-appsync').default;
-
-/*
+import AWSAppSyncClient, { AUTH_TYPE, createAppSyncLink } from 'aws-appsync';
+import fetch from 'node-fetch';
+import { createHttpLink } from 'apollo-link-http';
+import { expect } from 'chai';
+const aws_config = require('../../src/aws-exports').default;
 const queries = require('../../src/graphql/queries');
 
-const client = new AWSAppSyncClient({
-  url: awsconfig.aws_appsync_graphqlEndpoint,
-  region: awsconfig.aws_appsync_region,
+const link = createHttpLink({ uri: '/graphql', fetch: fetch });
+
+const AppSyncConfig = {
+  url: aws_config.aws_appsync_graphqlEndpoint,
+  region: aws_config.aws_appsync_region,
   auth: {
     type: AUTH_TYPE.API_KEY,
-    apiKey: awsconfig.aws_appsync_apiKey
+    apiKey: aws_config.aws_appsync_apiKey,
   },
-  disableOffline: true
+  disableOffline: true,
+};
+
+const client = new AWSAppSyncClient(AppSyncConfig, {
+  link: createAppSyncLink({
+    ...AppSyncConfig,
+    resultsFetcherLink: link,
+  })
 });
-*/
+
 describe('EditObjects', () => {
   it('aaa', () => {
     expect(1).eq(2);

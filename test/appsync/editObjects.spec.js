65afcee7bf5739d9033c2d464821b33da18b86d2
ishibashi_y <ishibashi_y@worksap.co.jp>
2019-06-25 17:09:35 +0900

    test: babel the test

--- test/appsync/editObjects.spec.js
+++ test/appsync/editObjects.spec.js
@@ -1,9 +1,11 @@
 import AWSAppSyncClient, { AUTH_TYPE, createAppSyncLink } from 'aws-appsync';
 import fetch from 'node-fetch';
+import gql from 'graphql-tag';
 import { createHttpLink } from 'apollo-link-http';
 import { expect } from 'chai';
 const aws_config = require('../../src/aws-exports').default;
 const queries = require('../../src/graphql/queries');
+const mutations = require('../../src/graphql/mutations');
 
 const link = createHttpLink({ uri: '/graphql', fetch: fetch });
 
@@ -24,8 +26,16 @@ const client = new AWSAppSyncClient(AppSyncConfig, {
   })
 });
 
+// This test is not working now. Put ApiKey on the header.
 describe('EditObjects', () => {
-  it('aaa', () => {
-    expect(1).eq(2);
+  it('should add an edit object', async () => {
+    const result = await client.mutate({
+      mutation: gql(mutations.addEditObject),
+      variables: {
+        floorId: 'test',
+      }
+    });
+
+    console.log(JSON.stringify(result));
   });
 });

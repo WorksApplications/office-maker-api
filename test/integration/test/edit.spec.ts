6a9d4a15d296d0fb290b9c6f78075fa3ad0af5b9
ishibashi_y <ishibashi_y@worksap.co.jp>
2019-07-05 14:56:27 +0900

    test: floor publish

--- test/integration/test/edit.spec.ts
+++ test/integration/test/edit.spec.ts
@@ -135,7 +135,7 @@ describe('Floor edit', () => {
     ).not.to.undefined;
   });
 
-  it('should not create show the deleted object', async () => {
+  it('should not show the deleted object', async () => {
     await axios.patch(
       `${endpoint.restApi.url}/objects`,
       [
@@ -183,4 +183,81 @@ describe('Floor edit', () => {
       }
     });
   });
+});
+
+describe('Floor publish', () => {
+  const floorId = uuid();
+  const objectId = uuid();
+
+  before(async () => {
+    await axios.put(
+      `${endpoint.restApi.url}/floors/${floorId}/edit`,
+      {
+        id: floorId,
+        name: 'New floor',
+        ord: 507,
+        realWidth: 10,
+        realHeight: 7,
+        temporary: false,
+        width: 7200,
+        height: 4560,
+        flipImage: false,
+        image: null
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${endpoint.systemJWT}`
+        }
+      }
+    );
+
+    await axios.patch(
+      `${endpoint.restApi.url}/objects`,
+      [
+        {
+          flag: 'added',
+          result: 'success',
+          object: {
+            backgroundColor: '#eee',
+            id: objectId,
+            floorId: floorId,
+            width: 0,
+            height: 0,
+            x: 0,
+            y: 0
+          }
+        }
+      ],
+      {
+        headers: {
+          Authorization: `Bearer ${endpoint.systemJWT}`
+        }
+      }
+    );
+  });
+
+  it('should publish the floor', async () => {
+    await axios.put(`${endpoint.restApi.url}/floors/${floorId}/public`, null, {
+      headers: {
+        Authorization: `Bearer ${endpoint.systemJWT}`
+      }
+    });
+  });
+
+  it('should show the floor in listFloors response', async () => {
+    const result = (await axios.get(`${endpoint.restApi.url}/floors`, {
+      headers: {
+        Authorization: `Bearer ${endpoint.systemJWT}`
+      }
+    })).data;
+
+    expect(result).to.be.an('array');
+    expect(result.length).to.not.equal(0);
+
+    const flat = arr => [].concat(...arr);
+    expect(flat(result).find(floor => floor && floor.id == floorId)).not.to
+      .undefined;
+  });
+
+  // check if the guest can see the published floor
 });
